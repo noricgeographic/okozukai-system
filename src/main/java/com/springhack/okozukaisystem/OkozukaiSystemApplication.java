8c3a93b691586c2d839ba8b8afeacfa47d0ff471
noric <info@noricgepgraphic.com>
2022-01-22 10:20:27 +0900

    spring initializr

---
+++ src/main/java/com/springhack/okozukaisystem/OkozukaiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springhack.okozukaisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkozukaiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkozukaiSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springhack/okozukaisystem/OkozukaiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springhack.okozukaisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkozukaiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
